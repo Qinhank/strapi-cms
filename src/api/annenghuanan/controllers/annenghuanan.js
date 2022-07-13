966c70c35f8c769e8edba886c8377024d6b81a6c
tx-hank <hank@tx.com>
2022-07-13 10:52:54 +0800

    u

---
+++ src/api/annenghuanan/controllers/annenghuanan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  annenghuanan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::annenghuanan.annenghuanan');
---
+++ src/api/annenghuanan/services/annenghuanan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * annenghuanan service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::annenghuanan.annenghuanan');
